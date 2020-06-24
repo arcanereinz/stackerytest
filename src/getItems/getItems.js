029018194edc1e8d36f7e42353849732bf92010f
Nang Chan <nchan@kixie.com>
2020-06-24 11:39:14 -0700

    IP check

--- src/getItems/getItems.js
+++ src/getItems/getItems.js
@@ -1,21 +1,28 @@
 const AWS = require('aws-sdk');
+const axios = require('axios');
+const url = 'http://checkip.amazonaws.com/';
 
 exports.handler = async (event, context) => {
   // Use dynamodb to get items from the Items table
   const dynamodb = new AWS.DynamoDB.DocumentClient();
   const params = {
-    TableName: process.env.TABLE_NAME
+    TableName: process.env.TABLE_NAME,
   };
 
   let allItems = [];
+  let axiosResponse;
 
   try {
     console.log(`Getting data from table ${process.env.TABLE_NAME}.`);
     const items = await dynamodb.scan(params).promise(); // get items from DynamoDB
     items.Items.forEach((item) => allItems.push(item)); // put contents in an array for easier parsing
-    allItems.forEach(item => console.log(`Item ${item.id}: ${item.content}\n`)); // log the contents
+    allItems.forEach((item) => console.log(`Item ${item.id}: ${item.content}\n`)); // log the contents
+
+    axiosResponse = await axios(url);
   } catch (error) {
-    console.log(`Error getting data from table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`);
+    console.log(
+      `Error getting data from table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`,
+    );
     throw new Error(error); // stop execution if data from dynamodb not available
   }
 
@@ -23,9 +30,12 @@ exports.handler = async (event, context) => {
   const response = {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: `${allItems.length} items found`
+    body: JSON.stringify({
+      message: `${allItems.length} items found`,
+      location: axiosResponse.data,
+    }),
   };
 
   return response;
